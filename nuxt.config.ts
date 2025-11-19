79164385f6012923b08d6720fb964c90f398f51c
Rafael Silva <rafaelsilvar221@gmail.com>
2025-11-19 20:10:38 -0300

    chore: update package.json to enhance linting and formatting tools
    - Updated lint command to use eslint on the entire project
    - lint:fix command for automatic lint fixing
    - Introduced format and format:fix commands for prettier checks and fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,86 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-11-17',
-  
+  ssr: true,
+  future: {
+    compatibilityVersion: 4
+  },
   modules: [
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/eslint-module',
+    '@pinia/nuxt',
+    '@nuxt/eslint',
+    [
+      '@nuxtjs/i18n',
+      {
+        locales: [
+          { code: 'es', language: 'es-ES', file: 'es.json', name: 'Español' },
+          { code: 'fr', language: 'fr-FR', file: 'fr.json', name: 'Français' }
+        ],
+        lazy: true,
+        langDir: 'locales',
+        defaultLocale: 'es',
+        strategy: 'prefix_except_default',
+        detectBrowserLanguage: {
+          useCookie: true,
+          cookieKey: 'i18n_redirected',
+          redirectOn: 'root'
+        }
+      }
+    ]
   ],
-
-  css: [
-    '~/assets/css/base.css',
-  ],
-});
+  css: ['~/assets/css/base.css'],
+  nitro: {
+    compressPublicAssets: true,
+    minify: true,
+    experimental: {
+      wasm: true
+    }
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'es-ES'
+      },
+      link: [
+        {
+          rel: 'preload',
+          href: '/fonts/Montserrat/static/Montserrat-Regular.ttf',
+          as: 'font',
+          type: 'font/ttf',
+          crossorigin: 'anonymous'
+        },
+        {
+          rel: 'preload',
+          href: '/fonts/Montserrat/static/Montserrat-Bold.ttf',
+          as: 'font',
+          type: 'font/ttf',
+          crossorigin: 'anonymous'
+        }
+      ]
+    }
+  },
+  postcss: {
+    plugins: {
+      'postcss-import': {},
+      'tailwindcss/nesting': {},
+      tailwindcss: {},
+      autoprefixer: {},
+      ...(process.env.NODE_ENV === 'production'
+        ? {
+            cssnano: {
+              preset: [
+                'default',
+                {
+                  discardComments: { removeAll: true },
+                  normalizeWhitespace: true,
+                  colormin: true,
+                  minifyFontValues: true,
+                  minifySelectors: true
+                }
+              ]
+            }
+          }
+        : {})
+    }
+  }
+})
