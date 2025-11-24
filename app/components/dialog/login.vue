d8fc672ea11254d41db782298a3819e8e9e8a1d8
Rafael Silva <rafaelsilvar221@gmail.com>
2025-11-24 10:59:56 -0300

    feat: dialog component for the login flow

---
+++ app/components/dialog/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <DialogRoot v-model:open="open">
+    <DialogPortal>
+      <DialogOverlay
+        class="fixed inset-0 bg-black/50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
+      />
+      <DialogContent
+        class="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white shadow-lg w-full max-w-[900px] max-h-[600px] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95"
+      >
+        <div class="grid md:grid-cols-2">
+          <NuxtImg
+            src="/images/webp/authentication-image-login.webp"
+            class="w-full max-h-[600px]"
+          />
+          <div class="w-full h-full bg-green-100"></div>
+        </div>
+      </DialogContent>
+    </DialogPortal>
+  </DialogRoot>
+</template>
+<script setup lang="ts">
+const open = defineModel<boolean>('open')
+</script>
